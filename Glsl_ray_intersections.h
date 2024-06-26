e211431acbfce6698c43eb010812b16d7fd28219
Dan Malta <5231886+DanIsraelMalta@users.noreply.github.com>
2024-06-26 16:10:48 +0300

    Update Glsl_ray_intersections to use glsl concepts instead of implementation where possible

--- Glsl_ray_intersections.h
+++ Glsl_ray_intersections.h
@@ -32,120 +32,120 @@ namespace RayIntersections {
     /**
     * \brief return distance to closest intersection point betweeb ray and disk, otherwise return -1
     *        notice that intersection is calculated only along the positive side of the disk (according to the normal)
-    * @param {Vector3,        in}  ray origin
-    * @param {Vector3,        in}  ray direction (should be normalized; not tested for normalization)
-    * @param {Vector3,        in}  disk center
-    * @param {Vector3,        in}  disk normal (should be normalized
-    * @param {floating_point, in}  disk radius
-    * @param {floating_point, out} distance to closest intersection point, along ray, from ray origin
+    * @param IFixedVector, in}  ray origin
+    * @param IFixedVector, in}  ray direction (should be normalized; not tested for normalization)
+    * @param IFixedVector, in}  disk center
+    * @param IFixedVector, in}  disk normal (should be normalized
+    * @param {value_type,  in}  disk radius
+    * @param {value_type,  out} distance to closest intersection point, along ray, from ray origin
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr T disk_intersect(const GLSL::Vector3<T>& ro, const GLSL::Vector3<T>& rd, const GLSL::Vector3<T>& c, const GLSL::Vector3<T>& n, T r) {
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires(std::is_floating_point_v<T> && (VEC::length() == 3))
+    constexpr T disk_intersect(const VEC& ro, const VEC& rd, const VEC& c, const VEC& n, T r) {
         assert(Extra::is_normalized(rd));
         assert(Extra::is_normalized(n));
 
-        const GLSL::Vector3<T> o{ ro - c };
+        const VEC o{ ro - c };
         const T dot{ GLSL::dot(rd, n) };
         if (dot > T{}) {
             return static_cast<T>(-1);
         }
 
         const T t{ -GLSL::dot(n, o) / dot };
-        const GLSL::Vector3<T> q{ o + rd * t };
+        const VEC q{ o + rd * t };
         return (GLSL::dot(q) < r * r) ? t : static_cast<T>(-1);
     }
 
     /**
     * \brief return the intersection between ray and triangle
-    * @param {Vector3, in}  ray origin
-    * @param {Vector3, in}  ray direction (should be normalized; not tested for normalization)
-    * @param {Vector3, in}  triangle vertex #0
-    * @param {Vector3, in}  triangle vertex #1
-    * @param {Vector3, in}  triangle vertex #2
-    * @param {Vector3, out} intersection point (in cartesian coordinate), if no intersection return vector filled with -1
+    * @param {IFixedVector, in}  ray origin
+    * @param {IFixedVector, in}  ray direction (should be normalized; not tested for normalization)
+    * @param {IFixedVector, in}  triangle vertex #0
+    * @param {IFixedVector, in}  triangle vertex #1
+    * @param {IFixedVector, in}  triangle vertex #2
+    * @param {IFixedVector, out} intersection point (in cartesian coordinate), if no intersection return vector filled with -1
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector3<T> triangle_intersect_cartesian(const GLSL::Vector3<T>& ro, const GLSL::Vector3<T>& rd, const GLSL::Vector3<T>& v0, const GLSL::Vector3<T>& v1, const GLSL::Vector3<T>& v2) {
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires(std::is_floating_point_v<T> && (VEC::length() == 3))
+    constexpr VEC triangle_intersect_cartesian(const VEC& ro, const VEC& rd, const VEC& v0, const VEC& v1, const VEC& v2) {
         assert(Extra::is_normalized(rd));
 
-        const GLSL::Vector3<T> v10{ v1 - v0 };
-        const GLSL::Vector3<T> v20{ v2 - v0 };
-        const GLSL::Vector3<T> n{ GLSL::cross(v10, v20) };
-        const GLSL::Vector3<T> normal{ GLSL::normalize(n) };
+        const VEC v10{ v1 - v0 };
+        const VEC v20{ v2 - v0 };
+        const VEC n{ GLSL::cross(v10, v20) };
+        const VEC normal{ GLSL::normalize(n) };
         const T dot{ GLSL::dot(normal, rd) };
         if (Numerics::areEquals(dot, T{})) {
-            return GLSL::Vector3<T>(static_cast<T>(-1.0));
+            return VEC(static_cast<T>(-1.0));
         }
 
         const T t{ GLSL::dot(-ro, normal) / dot };
-        return GLSL::Vector3<T>(ro + rd * t);
+        return VEC(ro + rd * t);
     }
 
     /**
     * \brief return the intersection between ray and triangle
-    * @param {Vector3, in}  ray origin
-    * @param {Vector3, in}  ray direction (should be normalized; not tested for normalization)
-    * @param {Vector3, in}  triangle vertex #0
-    * @param {Vector3, in}  triangle vertex #1
-    * @param {Vector3, in}  triangle vertex #2
-    * @param {Vector3, out} intersection point (distance to intersection, intersection point in barycentric coordinate), if no intersection return vector filled with -1
+    * @param {IFixedVector, in}  ray origin
+    * @param {IFixedVector, in}  ray direction (should be normalized; not tested for normalization)
+    * @param {IFixedVector, in}  triangle vertex #0
+    * @param {IFixedVector, in}  triangle vertex #1
+    * @param {IFixedVector, in}  triangle vertex #2
+    * @param {IFixedVector, out} intersection point (distance to intersection, intersection point in barycentric coordinate), if no intersection return vector filled with -1
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector3<T> triangle_intersect_barycentric(const GLSL::Vector3<T>& ro, const GLSL::Vector3<T>& rd, const GLSL::Vector3<T>& v0, const GLSL::Vector3<T>& v1, const GLSL::Vector3<T>& v2) {
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires(std::is_floating_point_v<T> && (VEC::length() == 3))
+    constexpr VEC triangle_intersect_barycentric(const VEC& ro, const VEC& rd, const VEC& v0, const VEC& v1, const VEC& v2) {
         assert(Extra::is_normalized(rd));
 
-        const GLSL::Vector3<T> v1v0{ v1 - v0 };
-        const GLSL::Vector3<T> v2v0{ v2 - v0 };
-        const GLSL::Vector3<T> rov0{ ro - v0 };
-        const GLSL::Vector3<T> n{ GLSL::cross(v1v0, v2v0) };
-        const GLSL::Vector3<T> q{ GLSL::cross(rov0, rd) };
+        const VEC v1v0{ v1 - v0 };
+        const VEC v2v0{ v2 - v0 };
+        const VEC rov0{ ro - v0 };
+        const VEC n{ GLSL::cross(v1v0, v2v0) };
+        const VEC q{ GLSL::cross(rov0, rd) };
         const T dot{ GLSL::dot(rd, n) };
         if (Numerics::areEquals(dot, T{})) {
-            return GLSL::Vector3<T>(static_cast<T>(-1.0));
+            return VEC(static_cast<T>(-1.0));
         }
 
         const T d{ static_cast<T>(1.0) / dot };
         const T u{ d * GLSL::dot(-q, v2v0) };
         const T v{ d * GLSL::dot(q, v1v0) };
         if ((Numerics::min(u, v) < T{}) || (u + v > static_cast<T>(1.0))) {
-            return GLSL::Vector3<T>(static_cast<T>(-1.0));
+            return VEC(static_cast<T>(-1.0));
         }
-        return GLSL::Vector3<T>(d* GLSL::dot(-n, rov0), u, v);
+        return VEC(d* GLSL::dot(-n, rov0), u, v);
     }
 
     /**
     * \brief return the intersection between ray and ellipse
-    * @param {Vector3, in}  ray origin
-    * @param {Vector3, in}  ray direction (should be normalized; not tested for normalization)
-    * @param {Vector3, in}  ellipse center
-    * @param {Vector3, in}  ellipse radii #1 ('u')
-    * @param {Vector3, in}  ellipse radii #1 ('v')
-    * @param {Vector3, out} intersection point (distance to intersection point, intersection point in uv coordinate), if no intersection return vector filled with -1
+    * @param {IFixedVector, in}  ray origin
+    * @param {IFixedVector, in}  ray direction (should be normalized; not tested for normalization)
+    * @param {IFixedVector, in}  ellipse center
+    * @param {IFixedVector, in}  ellipse radii #1 ('u')
+    * @param {IFixedVector, in}  ellipse radii #1 ('v')
+    * @param {IFixedVector, out} intersection point (distance to intersection point, intersection point in uv coordinate), if no intersection return vector filled with -1
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr auto ellipse_intersect(const GLSL::Vector3<T>& ro, const GLSL::Vector3<T>& rd, const GLSL::Vector3<T>& c, const GLSL::Vector3<T>& u, const GLSL::Vector3<T>& v) {
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires(std::is_floating_point_v<T> && (VEC::length() == 3))
+    constexpr VEC ellipse_intersect(const VEC& ro, const VEC& rd, const VEC& c, const VEC& u, const VEC& v) {
         assert(Extra::is_normalized(rd));
 
-        const GLSL::Vector3<T> q{ ro - c };
-        const GLSL::Vector3<T> n{ GLSL::cross(u, v) };
+        const VEC q{ ro - c };
+        const VEC n{ GLSL::cross(u, v) };
         const T dot{ GLSL::dot(rd, n) };
         if (Numerics::areEquals(dot, T{})) {
-            return GLSL::Vector3<T>(static_cast<T>(-1.0));
+            return VEC(static_cast<T>(-1.0));
         }
 
         const T t{ -GLSL::dot(n, q) / dot };
-        const GLSL::Vector3<T> qrdt{ q + rd * t };
+        const VEC qrdt{ q + rd * t };
         const T r{ GLSL::dot(u, qrdt) };
         const T s{ GLSL::dot(v, qrdt) };
 
         if (r * r + s * s > static_cast<T>(1)) {
-            return GLSL::Vector3<T>(static_cast<T>(-1));
+            return VEC(static_cast<T>(-1));
         }
-        return GLSL::Vector3<T>(t, s, r);
+        return VEC(t, s, r);
     }
 
     /**
@@ -189,7 +189,7 @@ namespace RayIntersections {
     **/
     template<typename T>
         requires(std::is_floating_point_v<T>)
-    constexpr auto ellipsoid_intersection(const GLSL::Vector3<T>& ro, const GLSL::Vector3<T>& rd, const GLSL::Vector3<T>& ra) {
+    constexpr GLSL::Vector2<T> ellipsoid_intersection(const GLSL::Vector3<T>& ro, const GLSL::Vector3<T>& rd, const GLSL::Vector3<T>& ra) {
         assert(Extra::is_normalized(rd));
         if (Numerics::areEquals(ra.x, T{}) ||
             Numerics::areEquals(ra.y, T{}) ||
