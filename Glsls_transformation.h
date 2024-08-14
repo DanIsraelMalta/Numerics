3f90e493d0a078de44ef4f4894d73b07ee81d727
Dan Malta <5231886+DanIsraelMalta@users.noreply.github.com>
2024-08-14 14:41:44 +0300

    refactor 'Transformation' functions to use generalized concepts where possible

--- Glsls_transformation.h
+++ Glsls_transformation.h
@@ -27,7 +27,7 @@
 #include "Glsl_extra.h"
 
 //
-// spatial relatied operations using vectors and matrices
+// spatial related operations using vectors and matrices
 //
 
 namespace Transformation {
@@ -36,26 +36,29 @@ namespace Transformation {
     * \brief efficient inverse of affine rigid transformation (assuming matrix is not singular)
     *        an affine rigid transformation matrix is a 4x4 matrix with first 3x3 portion holding
     *        rotation information and the fourth column holds the translation information.
-    * @param {Matrix4, in}  affine rigid transformation
-    * @param {Matrix4, out} invert of input matrix
+    * @param {IFixedCubicMatrix, in}  4x4 affine rigid transformation
+    * @param {IFixedCubicMatrix, out} invert of input matrix
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Matrix4<T> affine_rigid_inverse(const GLSL::Matrix4<T>& mat) noexcept {
+    template<GLSL::IFixedCubicMatrix MAT>
+        requires(MAT::columns() == 4)
+    constexpr MAT affine_rigid_inverse(const MAT& mat) noexcept {
+        using vec_t = typename MAT::vector_type;
+        using T = typename MAT::value_type;
+
         // transpose DCM
-        GLSL::Matrix4<T> inv{ GLSL::transpose(mat) };
+        MAT inv{ GLSL::transpose(mat) };
 
         // multiply translation by DCM transpose
-        const GLSL::Vector4<T> x{ mat[0] };
-        const GLSL::Vector4<T> y{ mat[1] };
-        const GLSL::Vector4<T> z{ mat[2] };
-        inv[3] = GLSL::Vector4<T>{ -GLSL::dot(z, x), -GLSL::dot(z, y), -GLSL::dot(z), static_cast<T>(1) };
+        const vec_t x{ mat[0] };
+        const vec_t y{ mat[1] };
+        const vec_t z{ mat[2] };
+        inv[3] = vec_t{ -GLSL::dot(z, x), -GLSL::dot(z, y), -GLSL::dot(z), static_cast<T>(1) };
 
-        // outout
+        // output
         return inv;
     }
-
-	/**
+    
+    /**
     * \brief generate look-at matrix (4x4)
     * @param {Vector3,                in}  origin/eye
     * @param {Vector3,                in}  target/center
@@ -67,7 +70,7 @@ namespace Transformation {
     constexpr GLSL::Matrix3<T> create_look_at_matrix(const GLSL::Vector3<T>& origin, const GLSL::Vector3<T>& target, const U roll_or_world_up) {
         const GLSL::Vector3<T> arbitrary = [roll_or_world_up]() {
             if constexpr (GLSL::is_fixed_vector_v<U>) {
-				assert(Extra::is_normalized(roll_or_world_up));
+                assert(Extra::is_normalized(roll_or_world_up));
                 return roll_or_world_up;
             } else {
                 return GLSL::Vector3<T>(std::sin(roll_or_world_up), std::cos(roll_or_world_up), T{});
@@ -85,18 +88,20 @@ namespace Transformation {
     /**
     * \brief return rotation axis and rotation angle of a rotation matrix.
     *        only works for non symmetric rotation matrices.
-    * @param {Matrix3,               in}  rotation matrix (not symmetric)
-    * @param {{Vector3, arithmetic}, out} {rotation axis, rotation angle [rad] cosine }
+    * @param {IFixedCubicMatrix,          in}  rotation matrix (not symmetric)
+    * @param {{IFixedVector, value_type}, out} {rotation axis, rotation angle [rad] cosine }
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr auto get_axis_angle_from_rotation_matrix(const GLSL::Matrix3<T>& mat) {
-        using out_t = struct { GLSL::Vector3<T> axis; T cosine; };
+    template<GLSL::IFixedCubicMatrix MAT>
+        requires(MAT::columns() == 3)
+    constexpr auto get_axis_angle_from_rotation_matrix(const MAT& mat) {
+        using VEC = typename MAT::vector_type;
+        using T = typename MAT::value_type;
+        using out_t = struct { VEC axis; T cosine; };
 
         assert(Extra::is_dcm_matrix(mat));
         assert(!Extra::is_symmetric(mat));
 
-        const GLSL::Vector3<T> axis(mat(1, 2) - mat(2, 1), mat(2, 0) - mat(0, 2), mat(0, 1) - mat(1, 0));
+        const VEC axis(mat(1, 2) - mat(2, 1), mat(2, 0) - mat(0, 2), mat(0, 1) - mat(1, 0));
         const T cosine{ (mat(0,0) + mat(1,1) + mat(2,2) - static_cast<T>(1)) / static_cast<T>(2) };
         return out_t{ GLSL::normalize(axis), cosine };
 
@@ -104,12 +109,12 @@ namespace Transformation {
 
     /**
     * \brief orient (rotate) a given vector towards a given direction
-    * @param {Vector2|Vector3, in}  vector to orient
-    * @param {Vector2|Vector3, in}  direction (must be normalized)
-    * @param {Vector2|Vector3, out} vector oriented according to direction
+    * @param {IFixedVector, in}  vector to orient
+    * @param {IFixedVector, in}  direction (must be normalized)
+    * @param {IFixedVector, out} vector oriented according to direction
     **/
     template<GLSL::IFixedVector VEC>
-        requires((VEC::length() == 2) || (VEC::length() == 3))
+        requires(VEC::length() <= 3)
     constexpr VEC orientate(const VEC& v, const VEC& dir) noexcept {
         using T = typename VEC::value_type;
         assert(Extra::is_normalized(dir));
@@ -124,14 +129,14 @@ namespace Transformation {
 
     /**
     * \brief generate a DCM (direct cosine matrix) matrix from axis and rotation angle
-    * @param {Vector3,        in}  axis
-    * @param {floating_point, in}  angle [rad]
-    * @param {Matrix3,        out} DCM (direct cosine matrix) matrix
+    * @param {IFixedVector,      in}  axis
+    * @param {value_type,        in}  angle [rad]
+    * @param {IFixedCubicMatrix, out} DCM (direct cosine matrix) matrix
     **/
     template<typename T>
         requires(std::is_floating_point_v<T>)
     constexpr GLSL::Matrix3<T> rotation_matrix_from_axis_angle(const GLSL::Vector3<T>& axis, const T angle) noexcept {
-	assert(Extra::is_normalized(axis));
+        assert(Extra::is_normalized(axis));
         const T sint{ std::sin(angle) };
         const T cost{ std::cos(angle) };
         const T icost{ static_cast<T>(1) - cost };
@@ -149,10 +154,10 @@ namespace Transformation {
     template<auto angle, class T = decltype(angle)>
         requires(std::is_floating_point_v<T>)
     constexpr GLSL::Matrix3<T> rotation_matrix_from_axis_angle(const GLSL::Vector3<T>& axis) noexcept {
+        assert(Extra::is_normalized(axis));
         constexpr T sint{ std::sin(angle) };
         constexpr T cost{ std::cos(angle) };
         constexpr T icost{ static_cast<T>(1) - cost };
-	assert(Extra::is_normalized(axis));
         const T axy{ axis.x * axis.y * icost };
         const T axz{ axis.x * axis.z * icost };
         const T ayz{ axis.y * axis.z * icost };
@@ -167,21 +172,21 @@ namespace Transformation {
 
     /**
     * \brief returns a point rotated around an axis. the rotation is centered
-    *        around the origin. (uses optimized version of rodrigues rotation formula)
-    * @param {Vector3,        in}  point
-    * @param {Vector3,        in}  axis (should be normalized)
-    * @param {floating_point, in}  angle [rad]
-    * @param {Vector3,        out} rotated point
+    *        around the origin. (uses optimized version of rodriguez rotation formula)
+    * @param {IFixedVector, in}  point
+    * @param {IFixedVector, in}  axis (should be normalized)
+    * @param {value_type,   in}  angle [rad]
+    * @param {IFixedVector, out} rotated point
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector3<T> rotate_point_around_axis(const GLSL::Vector3<T>& p, const GLSL::Vector3<T>& axis, const T angle) noexcept {
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires(VEC::length() == 3)
+    constexpr VEC rotate_point_around_axis(const VEC& p, const VEC& axis, const T angle) noexcept {
         assert(Extra::is_normalized(axis));
         return GLSL::mix(GLSL::dot(axis, p) * axis, p, std::cos(angle)) + GLSL::cross(axis, p) * std::sin(angle);
     }
-    template<auto angle, class T = decltype(angle)>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector3<T> rotate_point_around_axis(const GLSL::Vector3<T>& p, const GLSL::Vector3<T>& axis) noexcept {
+    template<auto angle, GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires(VEC::length() == 3 && std::is_same_v<T, decltype(angle)>)
+    constexpr VEC rotate_point_around_axis(const VEC& p, const VEC& axis) noexcept {
         constexpr T sinAngle{ std::sin(angle) };
         constexpr T cosAngle{ std::cos(angle) };
         assert(Extra::is_normalized(axis));
@@ -300,15 +305,15 @@ namespace Transformation {
 
     /**
     * \brief rotate a point using normalized quaternion
-    * @param {Vector4, in}  quaternion (normalized)
-    * @param {Vector3, in}  point to rotate
-    * @param {Vector3, out} rotate point
+    * @param {IFixedVector, in}  quaternion (normalized)
+    * @param {IFixedVector, in}  point to rotate
+    * @param {IFixedVector, out} rotate point
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector3<T> rotate_point_using_quaternion(const GLSL::Vector4<T>& quat, const GLSL::Vector3<T>& point) {
-        const GLSL::Vector3<T> axis{ Extra::get_quaternion_axis(quat) };
-        const GLSL::Vector3<T> temp{ static_cast<T>(2) * GLSL::cross(axis, point) };
+    template<GLSL::IFixedVector VEC4, GLSL::IFixedVector VEC3, class T = typename VEC3::value_type>
+        requires((VEC4::length() == 4) && (VEC3::length() == 3) && std::is_same_v<T, typename VEC4::value_type>)
+    constexpr VEC3 rotate_point_using_quaternion(const VEC4& quat, const VEC3& point) {
+        const VEC3 axis{ Extra::get_quaternion_axis(quat) };
+        const VEC3 temp{ static_cast<T>(2) * GLSL::cross(axis, point) };
         return (point + quat.w * temp + GLSL::cross(axis, temp));
     }
 }
