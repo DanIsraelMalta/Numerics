e0217d4aa13fce29e4e981972ca592b0c31e95ff
Dan Malta <5231886+DanIsraelMalta@users.noreply.github.com>
2024-10-22 15:15:34 +0300

    refactor for better usage of general concepts

--- Glsl_triangle.h
+++ Glsl_triangle.h
@@ -32,10 +32,10 @@ namespace Triangle {
 
     /**
     * \brief test if triangle is valid
-    * @param {Vector2|Vector3, in}  triangle vertex #0
-    * @param {Vector2|Vector3, in}  triangle vertex #1
-    * @param {Vector2|Vector3, in}  triangle vertex #2
-    * @param {bool,            out} true if triangle is valid, false otherwise
+    * @param {IFixedVector, in}  triangle vertex #0
+    * @param {IFixedVector, in}  triangle vertex #1
+    * @param {IFixedVector, in}  triangle vertex #2
+    * @param {bool,         out} true if triangle is valid, false otherwise
     **/
     template<GLSL::IFixedVector VEC>
         requires((VEC::length() == 2) || (VEC::length() == 3))
@@ -58,11 +58,11 @@ namespace Triangle {
 
     /**
     * \brief calculate the barycentric coordinates for a cartesian coordinate point in a triangle defined by vertices
-    * @param {Vector2|Vector3, in}  point
-    * @param {Vector2|Vector3, in}  triangle vertex #0
-    * @param {Vector2|Vector3, in}  triangle vertex #1
-    * @param {Vector2|Vector3, in}  triangle vertex #2
-    * @param {Vector3,         out} barycentric coordinates of a given point relative to triangle
+    * @param {IFixedVector, in}  point
+    * @param {IFixedVector, in}  triangle vertex #0
+    * @param {IFixedVector, in}  triangle vertex #1
+    * @param {IFixedVector, in}  triangle vertex #2
+    * @param {Vector3,      out} barycentric coordinates of a given point relative to triangle
     **/
     template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
         requires((VEC::length() == 2) || (VEC::length() == 3))
@@ -78,7 +78,7 @@ namespace Triangle {
         const T d11{ GLSL::dot(v1) };
         const T denom{ d00 * d11 - d01 * d01 };
         if (denom <= T{}) [[unlikely]] {
-            return GLSL::Vector3<T>(static_cast<T>(-1));
+            return VEC(static_cast<T>(-1));
         }
 
         const T d20{ GLSL::dot(v2, v0) };
@@ -91,13 +91,41 @@ namespace Triangle {
     }
 
     /**
+    * \brief given triangle (by its vertices) - return its barycentric coordinates
+    * @param {VEC,     in}  triangle vertex #0
+    * @param {VEC,     in}  triangle vertex #1
+    * @param {VEC,     in}  triangle vertex #2
+    * @param {Vector3, out} barycentric coordinates
+    **/
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires((VEC::length() == 2) || (VEC::length() == 3))
+    constexpr GLSL::Vector3<T> barycentric_from_cartesian(const VEC& a, const VEC& b, const VEC& c) {
+        assert(Triangle::is_valid(a, b, c));
+
+        const T daa{ GLSL::dot(a, a) };
+        const T dab{ GLSL::dot(a, b) };
+        const T dbb{ GLSL::dot(b, b) };
+        const T dca{ GLSL::dot(c, a) };
+        const T dcb{ GLSL::dot(c, b) };
+        const T denom{ daa * dbb - dab * dab };
+        if (denom <= T{}) [[unlikely]] {
+            return VEC(static_cast<T>(-1));
+        }
+
+        [[assume(denom != T{})]]
+        const T y{ (dbb * dca - dab * dcb) / denom };
+        const T z{ (daa * dcb - dab * dca) / denom };
+        return GLSL::Vector3<T>(static_cast<T>(1) - y - z, y, z);
+    }
+
+    /**
     * \brief check if a point is contained within a triangle
     *        uses Thomas Müller algorithm. faster than calculating triangle sign distance field at point.
-    * @param {Vector2|Vector3, in}  point
-    * @param {Vector2|Vector3, in}  triangle vertex #0
-    * @param {Vector2|Vector3, in}  triangle vertex #1
-    * @param {Vector2|Vector3, in}  triangle vertex #2
-    * @param {bool,            out} true if point is inside triangle, false otherwise
+    * @param {IFixedVector, in}  point
+    * @param {IFixedVector, in}  triangle vertex #0
+    * @param {IFixedVector, in}  triangle vertex #1
+    * @param {IFixedVector, in}  triangle vertex #2
+    * @param {bool,         out} true if point is inside triangle, false otherwise
     **/
     template<GLSL::IFixedVector VEC>
         requires((VEC::length() == 2) || (VEC::length() == 3))
@@ -123,46 +151,18 @@ namespace Triangle {
     }
 
     /**
-    * \brief given triangle (by its vertices) - return its barycentric coordinates
-    * @param {Vector2|Vector3, in}  triangle vertex #0
-    * @param {Vector2|Vector3, in}  triangle vertex #1
-    * @param {Vector2|Vector3, in}  triangle vertex #2
-    * @param {Vector3,         out} barycentric coordinates
-    **/
-    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
-        requires((VEC::length() == 2) || (VEC::length() == 3))
-    constexpr GLSL::Vector3<T> barycentric_from_cartesian(const VEC& a, const VEC& b, const VEC& c) {
-        assert(Triangle::is_valid(a, b, c));
-
-        const T daa{ GLSL::dot(a, a) };
-        const T dab{ GLSL::dot(a, b) };
-        const T dbb{ GLSL::dot(b, b) };
-        const T dca{ GLSL::dot(c, a) };
-        const T dcb{ GLSL::dot(c, b) };
-        const T denom{ daa * dbb - dab * dab };
-        if (denom <= T{}) [[unlikely]] {
-            return GLSL::Vector3<T>(static_cast<T>(-1));
-        }
-
-        [[assume(denom != T{})]]
-        const T y{ (dbb * dca - dab * dcb) / denom };
-        const T z{ (daa * dcb - dab * dca) / denom };
-        return GLSL::Vector3<T>(static_cast<T>(1) - y - z, y, z);
-    }
-
-    /**
     * \brief given a point and triangle, return point on triangle closest to the point
-    * @param {Vector2|Vector3,  in} point
-    * @param {Vector2|Vector3, in}  triangle vertex #0
-    * @param {Vector2|Vector3, in}  triangle vertex #1
-    * @param {Vector2|Vector3, in}  triangle vertex #2
-    * @param {Vector2|Vector3, out} closest corner
+    * @param {IFixedVector, in}  point
+    * @param {IFixedVector, in}  triangle vertex #0
+    * @param {IFixedVector, in}  triangle vertex #1
+    * @param {IFixedVector, in}  triangle vertex #2
+    * @param {IFixedVector, out} closest corner
     **/
     template<GLSL::IFixedVector VEC>
         requires((VEC::length() == 2) || (VEC::length() == 3))
     constexpr VEC closest_point(const VEC& p, const VEC& a, const VEC& b, const VEC& c) noexcept {
-        using T = typename VEC::value_type;
         assert(Triangle::is_valid(a, b, c));
+        using T = typename VEC::value_type;
 
         const VEC ab{ b - a };
         const VEC ac{ c - a };
@@ -216,22 +216,24 @@ namespace Triangle {
     }
 
     /**
-    * \brief orientation predicate for a point and a triangle
-    * @param {Vector3,    in}  point
-    * @param {Vector3,    in}  triangle vertex #0
-    * @param {Vector3,    in}  triangle vertex #1
-    * @param {Vector3,    in}  triangle vertex #2
-    * @param {value_type, in}  tolerance for point to be on triangle
-    * @param {int32_t,    out} -1/0/1 - point behind triangle / point on triangle / point in front of triangle
+    * \brief orientation predicate for a point and a 3D triangle
+    * @param {IFixedVector, in}  point
+    * @param {IFixedVector, in}  triangle vertex #0
+    * @param {IFixedVector, in}  triangle vertex #1
+    * @param {IFixedVector, in}  triangle vertex #2
+    * @param {value_type,   in}  tolerance for point to be on triangle
+    * @param {int32_t,      out} -1/0/1 - point behind triangle / point on triangle / point in front of triangle
     **/
-    template<class T>
-        requires(std::is_floating_point_v<T>)
-    constexpr std::int32_t point_triangle_orientation(const GLSL::Vector3<T>& p, const GLSL::Vector3<T>& a, const GLSL::Vector3<T>& b,
-                                                      const GLSL::Vector3<T>& c, const T eps = static_cast<T>(1e-10)) noexcept {
-        const GLSL::Matrix4<T> mat(GLSL::Vector4<T>(a, static_cast<T>(1)),
-                                   GLSL::Vector4<T>(b, static_cast<T>(1)),
-                                   GLSL::Vector4<T>(c, static_cast<T>(1)),
-                                   GLSL::Vector4<T>(p, static_cast<T>(1)));
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type>
+        requires(VEC::length() == 3)
+    constexpr std::int32_t point_triangle_orientation(const VEC& p, const VEC& a, const VEC& b,
+                                                      const VEC& c, const T eps = static_cast<T>(1e-10)) noexcept {
+        using vec4_t = next_vector_type<VEC>::vector_type;
+        using mat4_t = appropriate_matrix_type<vec4_t>::matrix_type;
+        const mat4_t mat(vec4_t(a, static_cast<T>(1)),
+                         vec4_t(b, static_cast<T>(1)),
+                         vec4_t(c, static_cast<T>(1)),
+                         vec4_t(p, static_cast<T>(1)));
         const T det{ GLSL::determinant(mat) };
         if (det < -eps) {
             return -1;
@@ -246,21 +248,21 @@ namespace Triangle {
 
     /**
     * \brief given two non-degenerate non-coplanar triangles - check if they intersect and return intersection point/segment
-    * @param {Vector3,            in}  triangle #1 vertex #0
-    * @param {Vector3,            in}  triangle #1 vertex #1
-    * @param {Vector3,            in}  triangle #1 vertex #2
-    * @param {Vector3,            in}  triangle #2 vertex #0
-    * @param {Vector3,            in}  triangle #2 vertex #1
-    * @param {Vector3,            in}  triangle #2 vertex #2
-    * @param {{Vector3, Vector3}, out} {point #1 of intersection segment, point #2 if intersection segment}
-    *                                  if no intersectio occures, both point #1 and point #2 will be infinity
+    * @param {IFixedVector,                 in}  triangle #1 vertex #0
+    * @param {IFixedVector,                 in}  triangle #1 vertex #1
+    * @param {IFixedVector,                 in}  triangle #1 vertex #2
+    * @param {IFixedVector,                 in}  triangle #2 vertex #0
+    * @param {IFixedVector,                 in}  triangle #2 vertex #1
+    * @param {IFixedVector,                 in}  triangle #2 vertex #2
+    * @param {{IFixedVector, IFixedVector}, out} {point #1 of intersection segment, point #2 if intersection segment}
+    *                                            if no intersectio occures, both point #1 and point #2 will be infinity
     **/
-    template<class T>
-        requires(std::is_floating_point_v<T>)
-    constexpr auto check_triangles_intersection(const GLSL::Vector3<T>& t1a, const GLSL::Vector3<T>& t1b, const GLSL::Vector3<T>& t1c,
-                                                const GLSL::Vector3<T>& t2a, const GLSL::Vector3<T>& t2b, const GLSL::Vector3<T>& t2c) noexcept {
-        using vec_t = GLSL::Vector3<T>;
-        using out_t = struct { vec_t p0; vec_t p1; };
+    template<GLSL::IFixedVector VEC>
+        requires(VEC::length() == 3)
+    constexpr auto check_triangles_intersection(const VEC& t1a, const VEC& t1b, const VEC& t1c,
+                                                const VEC& t2a, const VEC& t2b, const VEC& t2c) noexcept {
+        using T = typename VEC::value_type;
+        using out_t = struct { VEC p0; VEC p1; };
         constexpr T eps{ static_cast<T>(1e-10) };
 
         assert(Triangle::is_valid(t1a, t1b, t1c));
@@ -268,21 +270,21 @@ namespace Triangle {
 
         // housekeeping
         out_t out{
-            .p0 = vec_t(std::numeric_limits<T>::max()),
-            .p1 = vec_t(std::numeric_limits<T>::max())
+            .p0 = VEC(std::numeric_limits<T>::max()),
+            .p1 = VEC(std::numeric_limits<T>::max())
         };
 
         // lambda which permutes triangle vertices to the left
-        const auto permute_left = [](vec_t& a, vec_t& b, vec_t& c) {
-            vec_t temp(a);
+        const auto permute_left = [](VEC& a, VEC& b, VEC& c) {
+            VEC temp(a);
             a = MOV(b);
             b = MOV(c);
             c = MOV(temp);
         };
 
         // lambda which permutes triangle vertices to the right
-        const auto permute_right = [](vec_t& a, vec_t& b, vec_t& c) {
-            vec_t temp(c);
+        const auto permute_right = [](VEC& a, VEC& b, VEC& c) {
+            VEC temp(c);
             c = MOV(b);
             b = MOV(a);
             a = MOV(temp);
@@ -290,7 +292,7 @@ namespace Triangle {
 
         // lambda which permutes triangle vertex 'a' such that it would "lie on its side"
         // (oa/ob/oc = relative position to vertices a/b/c)
-        const auto make_a_alone = [&permute_left, &permute_right](vec_t& a, vec_t& b, vec_t& c,
+        const auto make_a_alone = [&permute_left, &permute_right](VEC& a, VEC& b, VEC& c,
                                      std::int32_t oa, std::int32_t ob, std::int32_t oc) {
             // Permute a, b, c so that a is alone on its side
             if (oa == ob) {
@@ -310,21 +312,21 @@ namespace Triangle {
         };
         
         // lambda to permute triangle 2 so its 'a' vertex lies in front of triangle 1
-        const auto make_a_positive = [](const vec_t& a1, const vec_t& a2, vec_t& b2, vec_t& c2) {
+        const auto make_a_positive = [](const VEC& a1, const VEC& a2, VEC& b2, VEC& c2) {
             const std::int32_t o{Triangle::point_triangle_orientation(a1, a2, b2, c2) };
             if (o < 0) {
-                vec_t temp{ c2 };
+                VEC temp{ c2 };
                 c2 = MOV(b2);
                 b2 = MOV(temp);
             }
         };
 
         // lambda to calculate intersection point between segment and a plane (defined by point and normal)
-        const auto get_segment_plane_intersect_point = [](const vec_t& a, const vec_t& b,
-                                                          const vec_t& p, const vec_t& n,
-                                                          vec_t& intersection) {
-            const vec_t u{ b - a };
-            const vec_t v{ a - p };
+        const auto get_segment_plane_intersect_point = [](const VEC& a, const VEC& b,
+                                                          const VEC& p, const VEC& n,
+                                                          VEC& intersection) {
+            const VEC u{ b - a };
+            const VEC v{ a - p };
             const T dot1{ GLSL::dot(n, u) };
             const T dot2{ GLSL::dot(n, v) };
             [[assume(dot1 > T{})]];
@@ -352,12 +354,12 @@ namespace Triangle {
         }
 
         // permute vertices
-        vec_t _t1a(t1a);
-        vec_t _t1b(t1b);
-        vec_t _t1c(t1c);
-        vec_t _t2a(t2a);
-        vec_t _t2b(t2b);
-        vec_t _t2c(t2c);
+        VEC _t1a(t1a);
+        VEC _t1b(t1b);
+        VEC _t1c(t1c);
+        VEC _t2a(t2a);
+        VEC _t2b(t2b);
+        VEC _t2c(t2c);
         make_a_alone(_t1a, _t1b, _t1c, o1a, o1b, o1c);
         make_a_alone(_t2a, _t2b, _t2c, o2a, o2b, o2c);
 
@@ -377,12 +379,12 @@ namespace Triangle {
         o2 = Triangle::point_triangle_orientation(_t2a, _t1a, _t1b, _t2c);
 
         // triangle normals
-        const vec_t n1{ GLSL::normalize(GLSL::cross(_t1c - _t1b, _t1a - _t1b)) };
-        const vec_t n2{ GLSL::normalize(GLSL::cross(_t2c - _t2b, _t2a - _t2b)) };
+        const VEC n1{ GLSL::normalize(GLSL::cross(_t1c - _t1b, _t1a - _t1b)) };
+        const VEC n2{ GLSL::normalize(GLSL::cross(_t2c - _t2b, _t2a - _t2b)) };
 
         // calculate intersection points
-        vec_t i0;
-        vec_t i1;
+        VEC i0;
+        VEC i1;
         if (o1 > 0) {
             if (o2 > 0) {
                 // Intersection: k i l j
