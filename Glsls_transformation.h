989ffdace087d2d3a56a59380719927e894adc63
Dan Malta <5231886+DanIsraelMalta@users.noreply.github.com>
2024-12-04 09:32:57 +0000

    rewrite functions in generic manner

--- Glsls_transformation.h
+++ Glsls_transformation.h
@@ -33,30 +33,31 @@
 namespace Transformation {
    
     /**
-    * \brief generate look-at matrix (4x4)
-    * @param {Vector3,                in}  origin/eye
-    * @param {Vector3,                in}  target/center
-    * @param {Vector3|floating_point, in}  either world up direction (normalized) or roll angle [rad]
-    * @param {Matrix3,                out} look at matrix
+    * \brief generate look-at matrix (3x3)
+    * @param {IFixedVector,            in}  origin/eye
+    * @param {IFixedVector,            in}  target/center
+    * @param {IFixedVector|value_type, in}  either world up direction (normalized) or roll angle [rad]
+    * @param {IFixedCubicMatrix,       out} look at matrix
     **/
-    template<typename T, typename U>
-        requires(std::is_floating_point_v<T> && (std::is_same_v<T, U> || GLSL::is_fixed_vector_v<U>))
-    constexpr GLSL::Matrix3<T> create_look_at_matrix(const GLSL::Vector3<T>& origin, const GLSL::Vector3<T>& target, const U roll_or_world_up) {
-        const GLSL::Vector3<T> arbitrary = [roll_or_world_up]() {
+    template<GLSL::IFixedVector VEC, class U, class T = typename VEC::value_type,
+             class MAT = appropriate_matrix_type<VEC>::matrix_type>
+        requires(VEC::length() <= 3 && std::is_floating_point_v<T> &&
+                 (std::is_same_v<T, U> || GLSL::is_fixed_vector_v<U>))
+    constexpr MAT create_look_at_matrix(const VEC& origin, const VEC& target, const U roll_or_world_up) {
+        const VEC arbitrary = [roll_or_world_up]() {
             if constexpr (GLSL::is_fixed_vector_v<U>) {
                 assert(Extra::is_normalized(roll_or_world_up));
                 return roll_or_world_up;
             } else {
-                return GLSL::Vector3<T>(std::sin(roll_or_world_up), std::cos(roll_or_world_up), T{});
+                return VEC(std::sin(roll_or_world_up), std::cos(roll_or_world_up), T{});
             }
         }();
 
-        const GLSL::Vector3<T> forward{ GLSL::normalize(origin - target) };
-        const GLSL::Vector3<T> right{ GLSL::normalize(GLSL::cross(arbitrary, forward)) };
-        const GLSL::Vector3<T> upper{ GLSL::cross(forward, right) };
+        const VEC forward{ GLSL::normalize(origin - target) };
+        const VEC right{ GLSL::normalize(GLSL::cross(arbitrary, forward)) };
+        const VEC upper{ GLSL::cross(forward, right) };
 
-        return GLSL::Matrix3<T>(right, upper, forward);
-
+        return MAT(right, upper, forward);
     }
 
     /**
@@ -107,9 +108,10 @@ namespace Transformation {
     * @param {value_type,        in}  angle [rad]
     * @param {IFixedCubicMatrix, out} DCM (direct cosine matrix) matrix
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Matrix3<T> rotation_matrix_from_axis_angle(const GLSL::Vector3<T>& axis, const T angle) noexcept {
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type,
+             class MAT = appropriate_matrix_type<VEC>::matrix_type>
+        requires(VEC::length() <= 3 && std::is_floating_point_v<T>)
+    constexpr MAT rotation_matrix_from_axis_angle(const VEC& axis, const T angle) noexcept {
         assert(Extra::is_normalized(axis));
         const T sint{ std::sin(angle) };
         const T cost{ std::cos(angle) };
@@ -121,13 +123,14 @@ namespace Transformation {
         const T say{ sint * axis.y };
         const T saz{ sint * axis.z };
 
-        return GLSL::Matrix3<T>(axis.x * axis.x * icost + cost, axy - saz,                      axz + say,
-                                axy + saz,                      axis.y * axis.y * icost + cost, ayz - sax,
-                                axz - say,                      ayz + sax,                      axis.z * axis.z * icost + cost);
+        return MAT(axis.x * axis.x * icost + cost, axy - saz,                      axz + say,
+                   axy + saz,                      axis.y * axis.y * icost + cost, ayz - sax,
+                   axz - say,                      ayz + sax,                      axis.z * axis.z * icost + cost);
     }
-    template<auto angle, class T = decltype(angle)>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Matrix3<T> rotation_matrix_from_axis_angle(const GLSL::Vector3<T>& axis) noexcept {
+    template<auto angle, GLSL::IFixedVector VEC, class T = typename VEC::value_type,
+             class MAT = appropriate_matrix_type<VEC>::matrix_type>
+        requires(VEC::length() <= 3 && std::is_floating_point_v<T> && std::is_same_v<T, decltype(angle)>)
+    constexpr MAT rotation_matrix_from_axis_angle(const VEC& axis) noexcept {
         assert(Extra::is_normalized(axis));
         constexpr T sint{ std::sin(angle) };
         constexpr T cost{ std::cos(angle) };
@@ -139,9 +142,9 @@ namespace Transformation {
         const T say{ sint * axis.y };
         const T saz{ sint * axis.z };
 
-        return GLSL::Matrix3<T>(axis.x * axis.x * icost + cost, axy - saz,                      axz + say,
-                                axy + saz,                      axis.y * axis.y * icost + cost, ayz - sax,
-                                axz - say,                      ayz + sax,                      axis.z * axis.z * icost + cost);
+        return MAT(axis.x * axis.x * icost + cost, axy - saz,                      axz + say,
+                   axy + saz,                      axis.y * axis.y * icost + cost, ayz - sax,
+                   axz - say,                      ayz + sax,                      axis.z * axis.z * icost + cost);
     }
 
     /**
@@ -157,7 +160,7 @@ namespace Transformation {
         return GLSL::mix(GLSL::dot(axis, p) * axis, p, std::cos(angle)) + GLSL::cross(axis, p) * std::sin(angle);
     }
     template<auto angle, GLSL::IFixedVector VEC, class T = typename VEC::value_type>
-        requires(VEC::length() == 3 && std::is_same_v<T, decltype(angle)>)
+        requires(VEC::length() == 3 && std::is_floating_point_v<T> && std::is_same_v<T, decltype(angle)>)
     constexpr VEC rotate_point_around_axis(const VEC& p, const VEC& axis) noexcept {
         constexpr T sinAngle{ std::sin(angle) };
         constexpr T cosAngle{ std::cos(angle) };
@@ -167,35 +170,39 @@ namespace Transformation {
 
     /**
     * \brief given axis and angle, return quaternion
-    * @param {Vector3,        in}  axis (should be normalized)
-    * @param {floating_point, in}  angle [rad]
-    * @param {Vector4,        out} quaternion
+    * @param {IFixedVector, in}  axis (should be normalized)
+    * @param {value_type,   in}  angle [rad]
+    * @param {IFixedVector, out} quaternion
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector4<T> create_quaternion_from_axis_angle(const GLSL::Vector3<T>& axis, const T angle) {
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type,
+             class out_t = next_vector_type<VEC>::vector_type>
+        requires(VEC::length() == 3)
+    constexpr out_t create_quaternion_from_axis_angle(const VEC& axis, const T angle) {
         assert(Extra::is_normalized(axis));
         const T halfAngle{ angle / static_cast<T>(2.0) };
-        return GLSL::Vector4<T>(std::sin(halfAngle) * axis, std::cos(halfAngle));
+        return out_t(std::sin(halfAngle) * axis, std::cos(halfAngle));
     }
-    template<auto angle, class T = decltype(angle)>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector4<T> create_quaternion_from_axis_angle(const GLSL::Vector3<T>& axis) {
+    template<auto angle, GLSL::IFixedVector VEC, class T = typename VEC::value_type,
+             class out_t = next_vector_type<VEC>::vector_type>
+        requires(VEC::length() == 3 && std::is_floating_point_v<T> && std::is_same_v<T, decltype(angle)>)
+    constexpr out_t create_quaternion_from_axis_angle(const VEC& axis) {
         assert(Extra::is_normalized(axis));
         constexpr T halfAngle{ angle / static_cast<T>(2.0) };
         constexpr T cosHalf{ std::cos(halfAngle) };
         constexpr T sinHalf{ std::sin(halfAngle) };
-        return GLSL::Vector4<T>(sinHalf * axis, cosHalf);
+        return out_t(sinHalf * axis, cosHalf);
     }
 
     /**
     * \brief create a quaternion from DCM matrix
-    * @param {Matrix3, in}  DCM matrix
-    * @param {Vector4, out} quaternion
+    * @param {IFixedCubicMatrix, in}  DCM matrix
+    * @param {IFixedVector,      out} quaternion
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Vector4<T> create_quaternion_from_rotation_matrix(const GLSL::Matrix3<T>& mat) {
+    template<GLSL::IFixedCubicMatrix MAT, class VEC3 = typename MAT::vector_type,
+             class VEC4 = next_vector_type<VEC3>::vector_type>
+        requires(MAT::columns() == 3)
+    constexpr VEC4 create_quaternion_from_rotation_matrix(const MAT& mat) {
+        using T = typename MAT::value_type;
         assert(Extra::is_orthonormal_matrix(mat));
 
         const T tr{static_cast<T>(1) + mat(0,0) + mat(1,1) + mat(2,2)};
@@ -207,10 +214,10 @@ namespace Transformation {
             [[assume(S > T{})]];
             const T Sinv{ static_cast<T>(1) / S };
 
-            return GLSL::Vector4<T>((mat(2,1) - mat(1,2)) * Sinv,
-                                    (mat(0,2) - mat(2,0)) * Sinv,
-                                    (mat(1,0) - mat(0,1)) * Sinv,
-                                    static_cast<T>(0.25) * S);
+            return VEC4((mat(2,1) - mat(1,2)) * Sinv,
+                        (mat(0,2) - mat(2,0)) * Sinv,
+                        (mat(1,0) - mat(0,1)) * Sinv,
+                        static_cast<T>(0.25) * S);
         } // DCM trace is zero, find largest diagonal element, and build quaternion using it
         else {
             // column 0
@@ -219,10 +226,10 @@ namespace Transformation {
                 [[assume(S > T{})]];
                 const T Sinv{ static_cast<T>(1) / S };
 
-                return GLSL::Vector4<T>(static_cast<T>(0.25) * S,
-                                        (mat(1, 0) + mat(0, 1)) * Sinv,
-                                        (mat(0, 2) + mat(2, 0)) * Sinv,
-                                        (mat(2, 1) - mat(1, 2)) * Sinv);
+                return VEC4(static_cast<T>(0.25) * S,
+                            (mat(1, 0) + mat(0, 1)) * Sinv,
+                            (mat(0, 2) + mat(2, 0)) * Sinv,
+                            (mat(2, 1) - mat(1, 2)) * Sinv);
             }   // Column 1
             else if (mat(1, 1) > mat(2, 2))
             {
@@ -230,10 +237,10 @@ namespace Transformation {
                 [[assume(S > T{})]];
                 const T Sinv{ static_cast<T>(1) / S };
 
-                return GLSL::Vector4<T>((mat(1, 0) + mat(0, 1)) * Sinv,
-                                        static_cast<T>(0.25) * S,
-                                        (mat(2, 1) + mat(1, 2)) * Sinv,
-                                        (mat(0, 2) - mat(2, 0)) * Sinv);
+                return VEC4((mat(1, 0) + mat(0, 1)) * Sinv,
+                            static_cast<T>(0.25) * S,
+                            (mat(2, 1) + mat(1, 2)) * Sinv,
+                            (mat(0, 2) - mat(2, 0)) * Sinv);
             }
             else
             {   // Column 2
@@ -241,24 +248,26 @@ namespace Transformation {
                 [[assume(S > T{})]];
                 const T Sinv{ static_cast<T>(1) / S };
 
-                return GLSL::Vector4<T>((mat(0, 2) + mat(2, 0)) * Sinv,
-                                        (mat(2, 1) + mat(1, 2)) * Sinv,
-                                        static_cast<T>(0.25) * S,
-                                        (mat(1, 0) - mat(0, 1)) * Sinv);
+                return VEC4((mat(0, 2) + mat(2, 0)) * Sinv,
+                            (mat(2, 1) + mat(1, 2)) * Sinv,
+                            static_cast<T>(0.25) * S,
+                            (mat(1, 0) - mat(0, 1)) * Sinv);
             }
         }
     }
 
     /**
     * \brief create a quaternion from DCM matrix
-    * @param {Vector4, in}  DCM matrix
-    * @param {Matrix3, out} quaternion
+    * @param {IFixedVector, in}  DCM matrix
+    * @param {IFixedCubicMatrix, out} quaternion
     **/
-    template<typename T>
-        requires(std::is_floating_point_v<T>)
-    constexpr GLSL::Matrix3<T> create_rotation_matrix_from_quaternion(const GLSL::Vector4<T>& quat) {
-        const T one{ static_cast<T>(1) };
-        const T two{ static_cast<T>(2) };
+    template<GLSL::IFixedVector VEC, class T = typename VEC::value_type,
+             class VEC3 = prev_vector_type<VEC>::vector_type,
+             class MAT = appropriate_matrix_type<VEC3>::matrix_type>
+        requires(VEC::length() == 4 && std::is_floating_point_v<T>)
+    constexpr MAT create_rotation_matrix_from_quaternion(const VEC& quat) {
+        constexpr T one{ static_cast<T>(1) };
+        constexpr T two{ static_cast<T>(2) };
         assert(Extra::is_normalized(quat));
         const T q0q1{ quat.x * quat.y };
         const T q0q2{ quat.x * quat.z };
@@ -269,9 +278,9 @@ namespace Transformation {
         const T q0Sqr{ quat.x * quat.x };
         const T q1Sqr{ quat.y * quat.y };
         const T q2Sqr{ quat.z * quat.z };
-        return GLSL::Matrix3<T>( one - two * (q2Sqr + q1Sqr), two * (q0q1 - q3q2),         two * (q0q2 + q3q1),
-                                 two * (q0q1 + q3q2),         one - two * (q0Sqr + q2Sqr), two * (q1q2 - q3q0),
-                                 two * (q0q2 - q3q1),         two * (q1q2 + q3q0),         one - two * (q0Sqr + q1Sqr) );
+        return MAT(one - two * (q2Sqr + q1Sqr), two * (q0q1 - q3q2),         two * (q0q2 + q3q1),
+                   two * (q0q1 + q3q2),         one - two * (q0Sqr + q2Sqr), two * (q1q2 - q3q0),
+                   two * (q0q2 - q3q1),         two * (q1q2 + q3q0),         one - two * (q0Sqr + q1Sqr) );
     }
 
     /**
@@ -281,7 +290,8 @@ namespace Transformation {
     * @param {IFixedVector, out} rotate point
     **/
     template<GLSL::IFixedVector VEC4, GLSL::IFixedVector VEC3, class T = typename VEC3::value_type>
-        requires((VEC4::length() == 4) && (VEC3::length() == 3) && std::is_same_v<T, typename VEC4::value_type>)
+        requires((VEC4::length() == 4) && (VEC3::length() == 3) && std::is_floating_point_v<T> &&
+                 std::is_same_v<T, typename VEC4::value_type>)
     constexpr VEC3 rotate_point_using_quaternion(const VEC4& quat, const VEC3& point) {
         const VEC3 axis{ quat.xyz };
         const VEC3 temp{ static_cast<T>(2) * GLSL::cross(axis, point) };
